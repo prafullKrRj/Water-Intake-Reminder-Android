399a3e490d38190892f9095dcff06f3ce96951c2
prafullKrRj <prafullkumar384@gmail.com>
2023-06-21 10:33:52 +0530

    Update

--- app/src/main/java/com/example/waterintakereminder/MainActivity.java
+++ app/src/main/java/com/example/waterintakereminder/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.waterintakereminder;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.utils.widget.ImageFilterButton;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+import androidx.core.view.GravityCompat;
 import androidx.core.widget.ImageViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -17,6 +19,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,9 +37,12 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
-    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
     public Toolbar toolbar;
     private boolean isToggled = false;
+    private ImageButton reminderToggleToolBar;
+    private ImageView logoOnToolBar;
+    private TextView textViewToolBar;
     @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +50,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
-        TextView textViewToolBar = findViewById(R.id.textViewToolBar);
+        textViewToolBar = findViewById(R.id.textViewToolBar);
         textViewToolBar.setText("Water Intake Reminder");
-
-        ImageView logoOnToolBar = findViewById(R.id.logoOnToolBar);
+        logoOnToolBar = findViewById(R.id.logoOnToolBar);
         logoOnToolBar.setImageResource(R.drawable.logo);
-
-        ImageButton reminderToggleToolBar = findViewById(R.id.reminderToggleToolBar);
+        reminderToggleToolBar = findViewById(R.id.reminderToggleToolBar);
         reminderToggleToolBar.setImageResource(R.drawable.baseline_alarm_on_24);
+
+
+        drawerLayout = findViewById(R.id.navigationDrawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openDrawer,
+                R.string.closeDrawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id==R.id.about){
+                    toHistoryFragment();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
         reminderToggleToolBar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,35 +95,37 @@ public class MainActivity extends AppCompatActivity {
         ft.add(R.id.container, new homeFragment());
         ft.commit();
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id==R.id.settings){
-                changeFragment(new SettingsFragment());
-                textViewToolBar.setText("Settings");
-                reminderToggleToolBar.setImageDrawable(null);
+                toSettingsFragment();
             }else if (id==R.id.analytics) {
-                changeFragment(new analyticsFragment());
-                textViewToolBar.setText("Analytics");
-                reminderToggleToolBar.setImageDrawable(null);
+                toAnalyticsFragment();
             }
             else if (id==R.id.alarmManager){
-                changeFragment(new alarmManagerFragment());
-                textViewToolBar.setText("Reminders");
-                reminderToggleToolBar.setImageDrawable(null);
+                toAlarmManagerFragment();
             }
             else if (id==R.id.history){
-                changeFragment(new historyFragment());
-                textViewToolBar.setText("History");
-                reminderToggleToolBar.setImageDrawable(null);
+                toHistoryFragment();
             }
             else{
                 changeFragment(new homeFragment());
                 textViewToolBar.setText("Water Intake Reminder");
                 reminderToggleToolBar.setImageResource((!isToggled) ? R.drawable.baseline_alarm_off_24 : R.drawable.baseline_alarm_on_24);
+
             }
             return true;
         });
+
+     /*   ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openDrawer,
+                R.string.closeDrawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();*/
     }
 
     @Override
@@ -122,4 +151,22 @@ public class MainActivity extends AppCompatActivity {
         ft.replace(R.id.container, fragment);
         ft.commit();
     }
+    private void toHistoryFragment(){
+        changeFragment(new historyFragment());
+        textViewToolBar.setText("History");
+        reminderToggleToolBar.setImageDrawable(null);
+    }private void toAnalyticsFragment(){
+        changeFragment(new analyticsFragment());
+        textViewToolBar.setText("Analytics");
+        reminderToggleToolBar.setImageDrawable(null);
+    }private void toSettingsFragment(){
+        changeFragment(new SettingsFragment());
+        textViewToolBar.setText("Settings");
+        reminderToggleToolBar.setImageDrawable(null);
+    }private void toAlarmManagerFragment(){
+        changeFragment(new alarmManagerFragment());
+        textViewToolBar.setText("Reminders");
+        reminderToggleToolBar.setImageDrawable(null);
+    }
+
 }
